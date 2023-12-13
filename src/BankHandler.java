ac0c757cb75783e2f6c14eeb0286f4f3a508e96d
Kristoffer Wilen <wilen.kristoffer@gmail.com>
2023-12-13 11:37:11 +0100

    added singleton pattern

--- src/BankHandler.java
+++ src/BankHandler.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class BankHandler implements BankManagerInterface {
@@ -5,9 +7,12 @@ public class BankHandler implements BankManagerInterface {
 
     private double balance;
     private Customer accountHolder;
-
-    public BankHandler(Customer accountHolder) {
+    private static Map<Customer, BankHandler> instances = new HashMap<>();
+    private BankHandler(Customer accountHolder) {
         this.accountHolder = accountHolder;
+    }
+    public static BankHandler getInstance(Customer accountHolder) {
+        return instances.computeIfAbsent(accountHolder, BankHandler::new);
     }
 
     public double getBalance() {
@@ -33,11 +38,30 @@ public class BankHandler implements BankManagerInterface {
             }
         }
 
-        public void userStartMenu() {
+    public void userStartMenu() {
 
-            Scanner scan = new Scanner(System.in);
+        System.out.println("Välkommen till Banken: Nuvarande saldo: " + getBalance());
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vad vill du göra? 1. Insättning 2. Uttag 3. Se Sparkonto");
+        int userChoiche = scanner.nextInt();
+
+        if(userChoiche == 1); {
+            System.out.println("Hur mycket vill du sätta in?");
+            double userDepositSum = scanner.nextDouble();
+            deposit(userDepositSum);
+        }
+        if (userChoiche == 2); {
+            System.out.println("Hur mycket vill du ta ut?");
+            double userDepositSum = scanner.nextDouble();
+            deposit(userDepositSum);
 
         }
+
+
+
+    }
     }
 
 
